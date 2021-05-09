01cf2626d8c8945e9a9d9a78198ef3a7807fc653
namdev1994 <n.lavhare1994@gmail.com>
2021-05-10 02:09:25 +0530

    Added: Check the firstName

--- main/java/com/UserRegistration/UserRegistration.java
+++ main/java/com/UserRegistration/UserRegistration.java
@@ -4,8 +4,27 @@ import java.util.regex.Matcher;
 import java.util.Scanner;
 
 public class UserRegistration {
+    static Scanner sc=new Scanner(System.in);
+    public static void isValidFirstName()
+    {
+        System.out.println("Enetr First Name : ");
+        String firstName=sc.nextLine();                       //Accept Input
+        String firstNamePattern="^([A-Z]{1})+[a-zA-Z]{2,}$"; //set Pattern
+        Pattern pattern= Pattern.compile(firstNamePattern);
+        Matcher matcher =pattern.matcher(firstName);
+        if(matcher.matches())                                //matches input With Pattern
+        {
+            System.out.println("Enter Last Name :");
+        }
+        else
+        {
+            System.out.println(" invalid First Name ");
+        }
+
+    }
     public static void main(String args[])
     {
         System.out.println("Welcome in User Registraion Program");
+        isValidFirstName(); //method call
     }
 }
