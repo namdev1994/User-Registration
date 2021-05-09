7a6dfecffff2bc766640fe42e4d30b46f1c18fb7
namdev1994 <n.lavhare1994@gmail.com>
2021-05-10 02:00:37 +0530

    Added:Welcome message in User Registration Program

---
+++ main/java/com/UserRegistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.UserRegistration;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Scanner;
+
+public class UserRegistration {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome in User Registraion Program");
+    }
+}
