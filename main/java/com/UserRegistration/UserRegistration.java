82582844c970d8c854de21d6c883b8ccf1bc259f
namdev1994 <n.lavhare1994@gmail.com>
2021-05-24 01:06:37 +0530

    Added:Create Lambda function

--- main/java/com/UserRegistration/UserRegistration.java
+++ main/java/com/UserRegistration/UserRegistration.java
@@ -2,104 +2,59 @@ package com.UserRegistration;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.Scanner;
+@FunctionalInterface
+interface IUserfunction{
+    boolean userValidation(String pattern, String input) throws UserRegistrationException;
+}
 
 public class UserRegistration {
-    static Scanner sc=new Scanner(System.in);
-    public static boolean isValidFirstName(String firstName) throws UserRegistrationException
-    {
-        String firstNamePattern="^([A-Z]{1})+[a-zA-Z]{2,}$"; //set Pattern
-        Pattern pattern= Pattern.compile(firstNamePattern);
-        Matcher matcher =pattern.matcher(firstName);
-        if(matcher.matches())                                //matches input With Pattern
-        {
-            System.out.println("Valid firstname");
-            return true;
-        }
-        else
-        {
-            System.out.println("Enter valid FirstName");
-           throw new UserRegistrationException("Enetr the valid Name");
+    public static String firstName, lastName, phoneNumber, password, email;
+    public static Scanner sc = new Scanner(System.in);
+    /**
+     * Here i have taken all the patterns which i have to compare with the userinputs.
+     */
+    private static final String FIRSTANDLASTNAME = "[A-Z][a-z]{3,}";
+    private static final String EMAIL = "^[a-zA-Z0-9]+(?:\\+*-*.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$";
+    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]?){8,}.*$";
+    private static final String PHONENUMBER = "^[0-9]{1,3} [0-9]{10}$";
 
-        }
-    }
-    public static boolean isValidLastName(String lastName) throws UserRegistrationException
-    {
-        String lastNamePattern="^([A-Z]{1})+[a-zA-Z]{2,}$";    //Set Pattern
-        Pattern pattern= Pattern.compile(lastNamePattern);
-        Matcher matcher =pattern.matcher(lastName);
-        if(matcher.matches())                                 //pattern Matches check
-        {
-            System.out.println("Vlaid lastname");
-            return true;
-        }
-        else
-        {
-            System.out.println("Enter valid LastName");
-            throw new UserRegistrationException("Enetr the valid Name");
-        }
-    }
-    public static boolean isValidEmailId(String email)throws UserRegistrationException
-    {
-        String emailPattern="^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*[@]{1}[a-z]+[.]{1}[a-z]{2,4}[.]*[a-z]*{2}$";//set pattern for email id
-        Pattern pattern= Pattern.compile(emailPattern);
-        Matcher matcher =pattern.matcher(email);
-        if(matcher.matches())                        //check Input and Pattern
-        {
-            System.out.println("valid Email");
-            return true;
-        }
-        else
-        {
-            System.out.println("Enter Valid Email");
-            throw new UserRegistrationException("Enetr the valid Email");
-        }
-    }
-    public static boolean isValidPhoneNumber(String phoneNumber)throws UserRegistrationException
-    {
-        String phoneNumberPattern = "^[0-9]{2}[ ]+[0-9]{10}";    //set pattern
-        Pattern pattern = Pattern.compile(phoneNumberPattern);
-        Matcher matcher = pattern.matcher(phoneNumber);
-        if (matcher.matches())                                 //Check input and Pattern
-        {
-            System.out.println("valid Phone number");
-            return true;
-        } else {
-            System.out.println("Enter valid phone Number");
-            throw new UserRegistrationException("Enetr the valid PhoneNumber");
-        }
-    }
-    public static boolean isValidPassword(String password)throws UserRegistrationException
-    {
-        String passwordPattern= "^(?=.*[0-9])(?=.*[@#$%^&!])(?=.*[A-Z])(?=.*[a-z]).{8,}$";    //set Pattern
-        Pattern pattern= Pattern.compile(passwordPattern);
-        Matcher matcher =pattern.matcher(password);
-        if(matcher.matches())                           //Check Input And Pattern
-        {
-            System.out.println("valid password");
-            return true;
-        }
-        else
-        {
-            System.out.println("Enetr valid password");
-            throw new UserRegistrationException("Enetr the valid Passwprd");
-        }
-    }
+
     public static void main(String args[]) throws UserRegistrationException {
-        System.out.println("Welcome in User Registraion Program");
-        System.out.println("Enter firstName");
-        String firstName= sc.next();
-        isValidFirstName(firstName);        //method call for firstName
-        System.out.println("Enter laststName");
-        String lastName=sc.next();
-        isValidLastName(lastName);        //method call for lastname
-        System.out.println("Enter Email");
-        String email=sc.next();
-        isValidEmailId(email);        //method call for email
-        System.out.println("Enter Phone Number");
-        String phoneNumber=sc.next();
-        isValidPhoneNumber(phoneNumber);   //method call for phone number
-        System.out.println("Enter password");
-        String password=sc.next();
-        isValidPassword(password);     //method call for password
+
+        IUserfunction userRegistration=(checkPattern, givenInput)->{    // Lambda function
+            Pattern pattern= Pattern.compile(checkPattern);
+            Matcher matcher =pattern.matcher(givenInput);
+            if(matcher.matches())                                //matches input With Pattern
+            {
+                return true;
+            }
+            else
+            {
+                throw new UserRegistrationException("Enetr the valid Input");
+            }
+        };
+        System.out.println("Enter Your Firstname: ");
+       // firstName=sc.nextLine();
+        userRegistration.userValidation(FIRSTANDLASTNAME,sc.nextLine());
+
+        System.out.println("Enter Your Lastname: ");
+       // lastName=sc.nextLine();
+        userRegistration.userValidation(FIRSTANDLASTNAME,sc.nextLine());
+
+        System.out.println("Enter Your Email-Id: ");
+        //email=sc.nextLine();
+        userRegistration.userValidation(EMAIL,sc.nextLine());
+
+        System.out.println("Enter Your Phone Number: ");
+        //phoneNumber=sc.nextLine();
+        userRegistration.userValidation(PHONENUMBER,sc.nextLine());
+
+        System.out.println("Enter Your Password: ");
+        //password=sc.nextLine();
+        userRegistration.userValidation(PASSWORD,sc.nextLine());
+
     }
+
+
 }
+
--- test/java/com/UserRegistration/UserRegistrationTest.java
+++ test/java/com/UserRegistration/UserRegistrationTest.java
@@ -3,100 +3,97 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
+
     @Test
     public void testFirstName() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean suraj = false;
-        try {
-            suraj = userRegistration.isValidFirstName("Suraj");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(true,suraj);
+        boolean check = false;
+        check = userRegistration.isValidName("Saurabh");
+        Assertions.assertTrue(check);
     }
-
-    @Test
-    public void testfirstNameFalse() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean suraj = false;
-        try {
-            suraj = userRegistration.isValidFirstName("Suraj");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(false,suraj);
-    }
-
-    @Test
-    public void testLastNameTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean suraj = false;
-        try {
-            suraj = userRegistration.isValidLastName("Rathod");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(true,suraj);
-    }
-
-    @Test
-    void testLastNameFalse() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean suraj = false;
-        try {
-            suraj = userRegistration.isValidLastName("rathod");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(false,suraj);
-    }
-
-    @Test
-    void testEmailIdTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean email = false;
-        try {
-            email = userRegistration.isValidEmailId("namdev@gmail.cm");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(true,email);
-    }
-
-    @Test
-    void testEmailIdFalse() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean email = false;
-        try {
-            email = userRegistration.isValidEmailId("namdev@gmail.cm");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(false,email);
-
-    }
-
-    @Test
-    void testPhoneNumberTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean email = false;
-        try {
-            email = userRegistration.isValidPhoneNumber("91 8080808080");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(true,email);
-    }
-
-    @Test
-    void testPhoneNumberFalse() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean email = false;
-        try {
-            email = userRegistration.isValidPassword("918080808080");
-        } catch (UserRegistrationException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(true,email);
-    }
+//
+//    @Test
+//    public void testfirstNameFalse() {
+//        UserRegistration userRegistration = new UserRegistration();
+//        boolean suraj = false;
+//        try {
+//            suraj = userRegistration.isValidFirstName("Suraj");
+//        } catch (UserRegistrationException e) {
+//            e.printStackTrace();
+//        }
+//        Assertions.assertEquals(false,suraj);
+//    }
+//
+//    @Test
+//    public void testLastNameTrue() {
+//        UserRegistration userRegistration = new UserRegistration();
+//        boolean suraj = false;
+//        try {
+//            suraj = userRegistration.isValidLastName("Rathod");
+//        } catch (UserRegistrationException e) {
+//            e.printStackTrace();
+//        }
+//        Assertions.assertEquals(true,suraj);
+//    }
+//
+//    @Test
+//    void testLastNameFalse() {
+//        UserRegistration userRegistration = new UserRegistration();
+//        boolean suraj = false;
+//        try {
+//            suraj = userRegistration.isValidLastName("rathod");
+//        } catch (UserRegistrationException e) {
+//            e.printStackTrace();
+//        }
+//        Assertions.assertEquals(false,suraj);
+//    }
+//
+//    @Test
+//    void testEmailIdTrue() {
+//        UserRegistration userRegistration = new UserRegistration();
+//        boolean email = false;
+//        try {
+//            email = userRegistration.isValidEmailId("namdev@gmail.cm");
+//        } catch (UserRegistrationException e) {
+//            e.printStackTrace();
+//        }
+//        Assertions.assertEquals(true,email);
+//    }
+//
+//    @Test
+//    void testEmailIdFalse() {
+//        UserRegistration userRegistration = new UserRegistration();
+//        boolean email = false;
+//        try {
+//            email = userRegistration.isValidEmailId("namdev@gmail.cm");
+//        } catch (UserRegistrationException e) {
+//            e.printStackTrace();
+//        }
+//        Assertions.assertEquals(false,email);
+//
+//    }
+//
+//    @Test
+//    void testPhoneNumberTrue() {
+//        UserRegistration userRegistration = new UserRegistration();
+//        boolean email = false;
+//        try {
+//            email = userRegistration.isValidPhoneNumber("91 8080808080");
+//        } catch (UserRegistrationException e) {
+//            e.printStackTrace();
+//        }
+//        Assertions.assertEquals(true,email);
+//    }
+//
+//    @Test
+//    void testPhoneNumberFalse() {
+//        UserRegistration userRegistration = new UserRegistration();
+//        boolean email = false;
+//        try {
+//            email = userRegistration.isValidPassword("918080808080");
+//        } catch (UserRegistrationException e) {
+//            e.printStackTrace();
+//        }
+//        Assertions.assertEquals(true,email);
+//    }
 }
